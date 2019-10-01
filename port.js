7e8ffbcf2d276798719230e8314e310db5923109
Hristo Dachev <tutini@gmail.com>
2019-10-01 10:39:26 +0300

    autoport facility

---
+++ port.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+
+const FN = 'parse';
+
+let src = fs.readFileSync('src/' + FN + '.ts', 'utf8');
+
+
+// Warn.
+
+if (/\.(map|filter|sort)\(/.test(src))
+    console.error('HAS higher order fns');
+
+if (/undefined/.test(src))
+    console.error('HAS undefined');
+
+if (/\|null/.test(src))
+    console.error('HAS |null');
+
+if (/\?:/.test(src))
+    console.error('HAS ?:');
+
+
+// Common replacers.
+
+src = src
+    .replace(/ === /g, ' == ')
+    .replace(/ !== /g, ' != ')
+    .replace(/\bfunction ([a-zA-Z]+)\b/g, (_, id) => 'fn ' + id)
+    .replace(/\btype ([a-zA-Z]+) =/g, (_, id) => 'struct ' + id)
+    .replace(/\blet ([a-zA-Z]+)/g,    (_, id) => 'mut ' + id)
+    .replace(/\bconst ([a-zA-Z]+)/g,  (_, id) => 'let ' + id)
+    .replace(/\b([a-zA-Z]+): number/g,  (_, id) => id + ': i32')
+    .replace(/\bboolean\b/g,  'bool')
+    .replace(/\bexport\b/g, 'pub')
+    ;
+
+
+// Done, do the rest yourself.
+
+fs.writeFileSync('src/fu/' + FN + '.fu', src);
