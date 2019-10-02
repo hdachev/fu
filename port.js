8042ccefb4e6d2c4905b3114fcd532b8bd3d4a4b
Hristo Dachev <tutini@gmail.com>
2019-10-02 18:08:37 +0300

    wip - autoport

--- port.js
+++ port.js
@@ -1,45 +1,61 @@
 const fs = require('fs');
+const FILES = [ 'parse', 'solve' ];
 
-const FN = 'parse';
+function port(file)
+{
+    console.log('\n' + file + ':');
 
-let src = fs.readFileSync('src/' + FN + '.ts', 'utf8');
-
-
-// Warn.
-
-if (/\.(map|filter|sort)\(/.test(src))
-    console.error('HAS higher order fns');
-
-if (/undefined/.test(src))
-    console.error('HAS undefined');
-
-if (/\|null/.test(src))
-    console.error('HAS |null');
-
-if (/\?:/.test(src))
-    console.error('HAS ?:');
+    let src = fs.readFileSync('src/' + file + '.ts', 'utf8');
 
 
-// Common replacers.
+    // Warn.
 
-src = src
-    .replace(/ === /g, ' == ')
-    .replace(/ !== /g, ' != ')
-    .replace(/\bfunction ([a-zA-Z]+)\b/g, (_, id) => 'fn ' + id)
-    .replace(/\btype ([a-zA-Z]+) =/g, (_, id) => 'struct ' + id)
-    .replace(/\blet ([a-zA-Z]+)/g,    (_, id) => 'mut ' + id)
-    .replace(/\bconst ([a-zA-Z]+)/g,  (_, id) => 'let ' + id)
-    .replace(/\b([a-zA-Z]+): number/g,  (_, id) => id + ': i32')
-    .replace(/\bboolean\b/g,  'bool')
-    .replace(/\bexport\b/g, 'pub')
+    if (/\.(map|filter|sort)\(/.test(src))
+        console.error('\tHAS higher order fns');
 
-    .replace(/\bTokenKind\b/, 'string')
-    .replace(/\bLexValue\b/, 'string')
-    .replace(/\bNodes\b/, 'Node[]')
+    if (/undefined/.test(src))
+        console.error('\tHAS undefined');
 
-    ;
+    if (/\|null/.test(src))
+        console.error('\tHAS |null');
+
+    if (/\?:/.test(src))
+        console.error('\tHAS ?:');
+
+    if (/\bswitch\b/.test(src))
+        console.error('\tHAS switch');
+
+    if (/\.\.\./.test(src))
+        console.error('\tHAS ...rest');
 
 
-// Done, do the rest yourself.
+    // Common replacers.
 
-fs.writeFileSync('src/fu/' + FN + '.fu', src);
+    src = src
+        .replace(/ === /g, ' == ')
+        .replace(/ !== /g, ' != ')
+        .replace(/\bfunction ([a-zA-Z]+)\b/g, (_, id) => 'fn ' + id)
+        .replace(/\btype ([a-zA-Z]+) =/g, (_, id) => 'struct ' + id)
+        .replace(/\blet ([a-zA-Z]+)/g,    (_, id) => 'mut ' + id)
+        .replace(/\bconst ([a-zA-Z]+)/g,  (_, id) => 'let ' + id)
+        .replace(/\b([a-zA-Z]+): number/g,  (_, id) => id + ': i32')
+        .replace(/\bboolean\b/g,  'bool')
+        .replace(/\bexport\b/g, 'pub')
+
+        .replace(/\bTokenKind\b/g, 'string')
+        .replace(/\bLexValue\b/g, 'string')
+        .replace(/\bNodes\b/g, 'Node[]')
+
+        .replace(/\b([a-zA-Z]+)(\[\])?\|null\b/g, (_, id) => id)
+
+        ;
+
+
+    // Done, do the rest yourself.
+
+    fs.writeFileSync('src/fu/' + file + '.fu', src);
+}
+
+FILES.forEach(port);
+
+console.log('');
