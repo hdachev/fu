14f789f88da0c24820b0d9e5b024a651edce5460
Hristo Dachev <tutini@gmail.com>
2019-10-13 19:30:16 +0300

    parser compiles & passes first test

--- src/prelude.ts
+++ src/prelude.ts
@@ -52,43 +52,30 @@ fn !!(a: &$T): bool __native_pure;
 
 // Assignment.
 
-fn =(a: &mut $T, b: $T)
-    case ($b -> @copy):     &mut $T __native_pure;
-    case ($b -> @move):     &mut $T __native_pure;
-
-fn ||=(a: &mut $T, b: $T)
-    case ($b -> @copy):     &mut $T __native_pure;
-    case ($b -> @move):     &mut $T __native_pure;
+fn   =(a: &mut $T, b: $T): &mut $T __native_pure;
+fn ||=(a: &mut $T, b: $T): &mut $T __native_pure;
 
 
 // Arrays.
 
-fn len(a: &$T[]):           i32 __native_pure;
+fn len (a: &$T[]):         i32  __native_pure;
+fn find(a: &$T[], b: &$T): i32  __native_pure;
+fn has (a: &$T[], b: &$T): bool __native_pure;
 
 fn [](a: &$T[], i: i32)
     case ($a -> &mut $T[]): &mut $T __native_pure;
     case ($a -> &    $T[]): &    $T __native_pure;
 
-fn push(a: &mut $T[], b: $T)
-    case ($b -> @copy): void __native_pure;
-    case ($b -> @move): void __native_pure;
+fn push   (a: &mut $T[], b: $T):              void __native_pure;
+fn unshift(a: &mut $T[], b: $T):              void __native_pure;
+fn insert (a: &mut $T[], i: i32, b: $T):      void __native_pure;
+fn concat (a: $T[], b: $T[]):                 $T[] __native_pure;
 
-fn unshift(a: &mut $T[], b: $T)
-    case ($b -> @copy): void __native_pure;
-    case ($b -> @move): void __native_pure;
+fn splice (a: &mut $T[], i: i32, count: i32): void __native_pure;
+fn pop    (a: &mut $T[]):                     void __native_pure;
 
-fn insert(a: &mut $T[], i: i32, b: $T)
-    case ($b -> @copy): void __native_pure;
-    case ($b -> @move): void __native_pure;
-
-fn splice(a: &mut $T[], i: i32, count: i32): void __native_pure;
-fn pop(a: &mut $T[]): void __native_pure;
-
-fn find(a: &$T[], b: &$T): i32  __native_pure;
-fn has(a: &$T[], b: &$T): bool __native_pure;
-
-fn move(a: &mut $T[], from: i32, to: i32): void __native_pure;
-fn sort(a: &mut $T[]): void __native_pure;
+fn move   (a: &mut $T[], from: i32, to: i32): void __native_pure;
+fn sort   (a: &mut $T[]):                     void __native_pure;
 
 
 // Strings.
@@ -108,7 +95,7 @@ fn <=(a: &string, b: &string):      bool        __native_pure;
 fn find(a: &string, b: &string):    i32         __native_pure;
 fn has(a: &string, b: &string):     bool        __native_pure;
 
-fn slice (a: &string, i0: i32, i1: i32): string  __native_pure;
+fn slice (a: &string, i0: i32, i1: i32): string __native_pure;
 fn substr(a: &string, i0: i32, i1: i32): string __native_pure;
 fn char  (a: &string, i0: i32): i32 __native_pure;
 
@@ -125,5 +112,10 @@ fn [](a: &Map($K, $V), b: &$K)
 fn throw(reason: string): never __native_pure;
 fn assert()             : never __native_pure;
 
+
+// Butt plugs.
+
+// TODO we should go for an any $B -> call stringify(b) macro.
+fn + (a: &string, b: i32): &string __native_pure;
 
 `;
