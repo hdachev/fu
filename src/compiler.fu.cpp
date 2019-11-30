9262a14a9ce857e592ef62ad56d7bdc2b5cbd2f7
Hristo Dachev <tutini@gmail.com>
2019-11-30 08:07:50 +0200

    profile

--- src/compiler.fu.cpp
+++ src/compiler.fu.cpp
@@ -4139,10 +4139,30 @@ struct sf_cpp_codegen
 
 fu_STR compile(const fu_STR& fname, const fu_STR& src, s_TEMP_Context& ctx)
 {
+    auto t0 = std::chrono::high_resolution_clock::now();
     s_LexResult res_lex = lex(src, fname);
+
+    auto t1 = std::chrono::high_resolution_clock::now();
     s_Node res_parse = parse(fname, res_lex.tokens);
+
+    auto t2 = std::chrono::high_resolution_clock::now();
     s_SolveResult res_solve = solve(res_parse, ctx);
+
+    auto t3 = std::chrono::high_resolution_clock::now();
     fu_STR cpp = cpp_codegen(res_solve.root, res_solve.scope, ctx);
+
+    auto t4 = std::chrono::high_resolution_clock::now();
+
+    auto t10 = std::chrono::duration_cast<std::chrono::microseconds>(t1 - t0).count();
+    auto t21 = std::chrono::duration_cast<std::chrono::microseconds>(t2 - t1).count();
+    auto t32 = std::chrono::duration_cast<std::chrono::microseconds>(t3 - t2).count();
+    auto t43 = std::chrono::duration_cast<std::chrono::microseconds>(t4 - t3).count();
+
+    std::cout << "    lex:     " << t10 << " µs" << std::endl;
+    std::cout << "    parse:   " << t21 << " µs" << std::endl;
+    std::cout << "    solve:   " << t32 << " µs" << std::endl;
+    std::cout << "    codegen: " << t43 << " µs" << std::endl;
+    
     return cpp;
 }
 
