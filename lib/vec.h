78fe46b9a04da8c2aab03f3760e3b4094ba05133
Hristo Dachev <tutini@gmail.com>
2019-11-30 12:08:38 +0200

    fbstring::find

--- lib/vec.h
+++ lib/vec.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "arc.h"
+#include "find.h"
 
 template <typename T>
 struct fu_VEC
@@ -943,33 +944,11 @@ struct fu_VEC
         return find(v.data(), v.size());
     }
 
-    fu_NEVER_INLINE i32 find(const T* i0, i32 s) const noexcept {
-        i32 my_size = size();
-        if (s <= 0 || s > my_size)
-            return s == 0 ? 0 : -1;
-
-        const T* i1     = i0 + s;
-        const T* start  = data();
-        const T* end    = start + (my_size - s);
-        const T* back   = end;
-
-        for (const T* i = start; i <= end; i++) {
-            if (*i != *i0)
-                continue;
-
-            back = i++;
-            for (const T* search = i0 + 1; search < i1; i++, search++) {
-                if (*i != *search) {
-                    i = back;
-                    goto continue_OUTER;
-                }
-            }
-
-            return int(back - start);
-            continue_OUTER:;
-        }
-
-        return -1;
+    fu_INL i32 find(const T* i0, i32 s) const noexcept {
+        return fbstring_lfind(
+            data(), size(),
+            i0, s,
+            fu_ZERO());
     }
 
     bool starts_with(const T* i0, i32 s) const noexcept {
