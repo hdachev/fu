7a39c2c6408244b1ba013e8e7096b69ffc31fc63
Hristo Dachev <tutini@gmail.com>
2020-12-09 10:16:35 +0200

    defer:ok & defer:err

--- include/fu/defer.h
+++ include/fu/defer.h
@@ -1,14 +1,17 @@
 #pragma once
 
+// defer_if_err & defer_if_ok
+#include <exception>
+
 /////////////////////////////////////////////
 
 #define fu_CONCAT_detail(a, b) a##b
 #define fu_CONCAT(a, b) fu_CONCAT_detail(a, b)
 #define fu_UNIQUE_ID() fu_CONCAT(fu_UNIQUE_ID_, __COUNTER__)
 
-/////////////////////////////////////////////
-
 namespace fu {
+
+/////////////////////////////////////////////
 
 template <typename F>
 struct defer
@@ -22,5 +25,39 @@ struct defer
 };
 
 #define fu_DEFER(x) fu::defer fu_UNIQUE_ID() { [&]() { x; } };
+
+/////////////////////////////////////////////
+
+template <typename F>
+struct defer_if_err
+{
+    F fn;
+    int ue;
+    ~defer_if_err() { if (ue != std::uncaught_exceptions()) fn(); }
+
+    defer_if_err(F fn) : fn(fn), ue(std::uncaught_exceptions()) {}
+    defer_if_err(const defer_if_err&) = delete;
+    void operator=(const defer_if_err&) = delete;
+};
+
+#define fu_DEFER_IF_ERR(x) fu::defer_if_err fu_UNIQUE_ID() { [&]() { x; } };
+
+/////////////////////////////////////////////
+
+template <typename F>
+struct defer_if_ok
+{
+    F fn;
+    int ue;
+    ~defer_if_ok() { if (ue == std::uncaught_exceptions()) fn(); }
+
+    defer_if_ok(F fn) : fn(fn), ue(std::uncaught_exceptions()) {}
+    defer_if_ok(const defer_if_ok&) = delete;
+    void operator=(const defer_if_ok&) = delete;
+};
+
+#define fu_DEFER_IF_OK(x) fu::defer_if_ok fu_UNIQUE_ID() { [&]() { x; } };
+
+/////////////////////////////////////////////
 
 } // namespace
