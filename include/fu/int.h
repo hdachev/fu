cb766db93f21f21e8023d5f4e471ac43f5e4c8ef
Hristo Dachev <tutini@gmail.com>
2021-11-26 20:16:42 +0200

    int.h

---
+++ include/fu/int.h
@@ -0,0 +1,13 @@
+#pragma once
+
+namespace fu {
+
+// Plain char, signed char, and unsigned char
+//  are three distinct types, collectively called
+//   narrow character types.
+
+typedef          char byte;
+typedef   signed char i8;
+typedef unsigned char u8;
+
+}
