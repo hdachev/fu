b8d85ee3f81d02621c7cce6a63d864238074b738
Hristo Dachev <tutini@gmail.com>
2021-12-03 18:45:25 +0200

    view_assign.h ok

--- include/fu/view.h
+++ include/fu/view.h
@@ -1,9 +1,9 @@
 #pragma once
 
 #include <cassert>
-#include <type_traits>
+#include <cstdint>
 
-#include "./util.h"
+#include "./inl.h"
 
 namespace fu {
 
@@ -27,7 +27,7 @@ struct view
         , m_size { size }
     {}
 
-    template <typename V, typename = decltype(*((const T**)1) = std::declval<V>().data())>
+    template <typename V, typename = decltype(*((const T**)1) = (*(V*)1).data())>
     fu_INL view(const V& vec) noexcept
         : m_data { vec.data() }
         , m_size { vec.size() }
@@ -304,44 +304,5 @@ fu_INL view_mut<Dest> view_of_mut(V&& src, const Dest&) noexcept
     return into_view_mut<Dest>(src);
 }
 
-
-//
-
-template <typename V0, typename V1,
-    typename T0 = typename V0::value_type,
-    typename T1 = typename V1::value_type>
-fu_INL void view_assign(V0& a, const V1& b) noexcept
-{
-    static_assert(
-        std::is_trivially_destructible<T0>::value &&
-        std::is_trivially_destructible<T1>::value,
-            "TODO view_assign: non-trivial view assign.");
-
-    static_assert(
-        sizeof(T0) % sizeof(T1) == 0 ||
-        sizeof(T1) % sizeof(T0) == 0,
-            "view_assign: neither T0/T1 is size-mod the other.");
-
-    auto s0 = a.size() * sizeof(T0);
-    auto s1 = b.size() * sizeof(T1);
-    assert(s0 == s1);
-
-    auto s  = s0 < s1 ? s0 : s1;
-    auto pd = reinterpret_cast<char*>       (a.data_mut());
-    auto ps = reinterpret_cast<const char*> (b.data()    );
-
-    assert((pd <= ps - s || pd >= ps + s) &&
-        "view_assign: mutptr aliasing, "
-        "TODO use memcpy instead of memmove.");
-
-    if (pd != ps && s)
-        std::memmove(pd, ps, s);
-}
-
-template <typename V0, typename V1>
-fu_INL void view_assign(V0&& a, const V1& b) noexcept
-{
-    return view_assign(a, b);
-}
 
 } // namespace
