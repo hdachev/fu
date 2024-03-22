ec8d3fc4aba14c61f45295a6a802aeef8f1b290b
Hristo Dachev <tutini@gmail.com>
2024-03-22 12:16:48 +0200

    node watch: _spawnTime + kill

--- watch.js
+++ watch.js
@@ -3,20 +3,34 @@ const path      = require('path');
 const fs        = require('fs');
 
 let _dirtyTime  = 0;
+let _spawnTime  = 0;
+
 let _dirty      = false;
 let _rebuild    = null;
 let _didRecycle = false;
+let _didBoot    = false;
+
+// TODO FIX we don't seem to be shutting down child processes on SIGTERM
+let KILL_EARLY  = false;
 
 function triggerRebuild()
 {
+    const now   = Date.now();
+
     if (_rebuild)
+    {
+        if (_didBoot && Math.abs(_spawnTime - now) > 1000 && KILL_EARLY)
+            _rebuild.kill();
+
         return;
+    }
 
     const cmd   = _didRecycle ? './cycle' : (_didRecycle = true, './recycle');
 
     // console.log('\n-----------\nRebuild', cmd, '...\n-----------\n');
 
     _dirty      = false;
+    _spawnTime  = now;
     _rebuild    = cp.spawn(cmd, {
         stdio: [process.stdin, process.stdout, process.stderr] });
 
@@ -32,6 +46,10 @@ function triggerRebuild()
         {
             // console.log('\n-----------\nBuild failed.\n-----------\n');
             return;
+        }
+        else
+        {
+            _didBoot = true;
         }
     });
 }
