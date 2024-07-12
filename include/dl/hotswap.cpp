7424e597f2b44d0c38692680f75ecb85acc585f7
Hristo Dachev <tutini@gmail.com>
2024-07-12 12:07:40 +0300

    hotswap: dlinfo, dladdr, FU_HOTSWAP_RELOADING + always -fPIC

--- include/dl/hotswap.cpp
+++ include/dl/hotswap.cpp
@@ -10,14 +10,22 @@
 
 #include <fu/vec.h>
 
+#define fu_HOTSWAP_use_rename_hack
+
+#if defined(fu_HOTSWAP_use_rename_hack) && !defined(RTLD_DI_LINKMAP)
+#define fu_HOTSWAP_use_dladdr
+#endif
+
 namespace
 {
 
     // My ubuntu refuses to reload the same lib.
 
-    constexpr bool RENAME_LIBS_RANDOMLY     = true;
-    constexpr bool LOAD_ON_BOOT             = false;
-              bool VERBOSE                  = !!getenv("fu_VERBOSE");
+    bool FU_VERBOSE             = !!getenv("FU_VERBOSE");
+    bool FU_HOTSWAP_RELOADING   = false;
+
+    bool FU_HOTSWAP_MANUAL_MODE = false;
+    bool FU_HOTSWAP_SIGNALLED   = false;
 
 
     //
@@ -27,6 +35,14 @@ namespace
         const char* filename        {};
               void* handle          {};
               void* handle_old      {};
+
+        #ifdef fu_HOTSWAP_use_rename_hack
+        const char* abs_path        {};
+
+            #ifdef fu_HOTSWAP_use_dladdr
+              void* dladdr_symbol   {};
+            #endif
+        #endif
     };
 
     struct Item
@@ -44,12 +60,17 @@ namespace
 
     //
 
-    bool try_reload(Item& item, const Loader& ld)
+    bool try_reload(Item& item, Loader& ld)
     {
         void* new_fnptr = dlsym(ld.handle, item.symbol);
         if (new_fnptr)
         {
-            if (VERBOSE)
+            #ifdef fu_HOTSWAP_use_dladdr
+            ld.dladdr_symbol = new_fnptr;
+            #endif
+
+            //
+            if (FU_VERBOSE)
             {
                 if (*item.slot == new_fnptr)
                     printf("HOTSWAP Got the same fnptr for `%s` in `%s`\n", item.symbol, ld.filename);
@@ -62,7 +83,7 @@ namespace
         }
         else
         {
-            if (VERBOSE)
+            if (FU_VERBOSE)
                 printf("HOTSWAP Could not find `%s` in `%s`\n", item.symbol, ld.filename);
         }
 
@@ -71,31 +92,77 @@ namespace
 
     bool try_reload(Loader& ld)
     {
+        // Don't register new symbols.
+        FU_HOTSWAP_RELOADING = true;
+
+        //
         void* new_handle {};
 
         // Try load.
         {
-            if (RENAME_LIBS_RANDOMLY)
+            #ifdef fu_HOTSWAP_use_rename_hack
+            if (!ld.abs_path)
+            {
+                // 2024: the stuff below works for both linux and macos,
+                //  but doesn't account for $ORIGIN, @rpath and co,
+                //   and just tries to rename the file in the local directory.
+                //
+                // What we're now doing is to initially load without renaming,
+                //  then use that loaded lib to query its abs_path via dlinfo/dladdr,
+                //   and then use that abspath to rename the lib before reloading it later.
+                //
+                #ifdef fu_HOTSWAP_use_dladdr
+                Dl_info link_map {};
+
+                if (ld.dladdr_symbol
+                        // On success dladdr() returns a **NONZERO** value.
+                        && dladdr(ld.dladdr_symbol, &link_map)
+                        && link_map.dli_fname)
+                {
+                    ld.abs_path = link_map.dli_fname;
+                }
+                #else
+                Link_map link_map {};
+
+                if (ld.handle
+                        // On success dlinfo() function returns 0.
+                        && !dlinfo(ld.handle, RTLD_DI_LINKMAP, &link_map)
+                        && link_map.l_name)
+                {
+                    ld.abs_path = link_map.l_name;
+                }
+                #endif
+                else
+                {
+                    if (FU_VERBOSE)
+                        printf("HOTSWAP Failed to resolve dlinfo/dladdr for `%s`\n", ld.filename);
+                }
+            }
+
+            if (ld.abs_path)
             {
                 static int unique_id = rand();
 
-                char tempname[1024];
-                snprintf(tempname, 1024, "%s%i", ld.filename, unique_id++);
+                char tempname[PATH_MAX];
+                snprintf(tempname, PATH_MAX, "%s%i", ld.filename, unique_id++);
 
-                // Linux can rebel, ignores dlclose and then returns the same handle on dlopen.
-                if (rename(ld.filename, tempname))
+                // Linux can rebel, ignores dlclose
+                //  and then returns the same handle on dlopen.
+                //
+                if (rename(ld.abs_path, tempname))
                 {
-                    if (VERBOSE)
-                        printf("HOTSWAP Cannot rename `%s` to `%s`: %i\n", ld.filename, tempname, errno);
+                    if (FU_VERBOSE)
+                        printf("HOTSWAP Cannot rename `%s` to `%s`: %i\n", ld.abs_path, tempname, errno);
 
                     goto OpenWithoutRenaming;
                 }
 
                 new_handle = dlopen(tempname, RTLD_NOW);
-                rename(tempname, ld.filename);
+                rename(tempname, ld.abs_path);
 
                 goto Done;
             }
+            #endif //fu_HOTSWAP_use_rename_hack
 
             OpenWithoutRenaming:
             new_handle = dlopen(ld.filename, RTLD_NOW);
@@ -106,21 +173,29 @@ namespace
         // Rotate.
         if (new_handle)
         {
-            if (VERBOSE)
+            if (FU_VERBOSE)
                 printf("HOTSWAP Reloaded `%s`\n", ld.filename);
 
             // TODO FIX crashes once every two/three reloads,
             //  will leak them for now.
             //
             // if (ld.handle_old)
+            // {
+            //     ld.abs_path = nullptr;
             //     dlclose(ld.handle_old);
+            // }
+            //
+            ld.handle_old       = ld.handle;
+            ld.handle           = new_handle;
 
-            ld.handle_old   = ld.handle;
-            ld.handle       = new_handle;
+            #ifdef fu_HOTSWAP_use_dladdr
+            ld.dladdr_symbol    = nullptr;
+            #endif
+
             return true;
         }
 
-        if (VERBOSE)
+        if (FU_VERBOSE)
             printf("HOTSWAP Could not reload `%s`\n", ld.filename);
 
         return false;
@@ -142,35 +217,32 @@ namespace
                     try_reload(item, lib);
     }
 
-    volatile bool manual_reload_mode    = false;
-    volatile bool manual_reload_needed  = false;
-
     void onManualReload()
     {
-        manual_reload_mode = true;
+        FU_HOTSWAP_MANUAL_MODE = true;
 
-        if (manual_reload_needed)
+        if (FU_HOTSWAP_SIGNALLED)
         {
-            if (VERBOSE)
+            if (FU_VERBOSE)
                 printf("HOTSWAP hotswap::here() called, reloading.\n");
 
-            manual_reload_needed = false;
+            FU_HOTSWAP_SIGNALLED = false;
             doReload();
         }
     }
 
     extern "C" void onSIGUSR1_reloadLibs(int /*signal*/)
     {
-        if (manual_reload_mode)
+        if (FU_HOTSWAP_MANUAL_MODE)
         {
-            if (VERBOSE)
+            if (FU_VERBOSE)
                 printf("HOTSWAP Received SIGUSR1, awaiting next call to hotswap::here().\n");
 
-            manual_reload_needed = true;
+            FU_HOTSWAP_SIGNALLED = true;
         }
         else
         {
-            if (VERBOSE)
+            if (FU_VERBOSE)
                 printf("HOTSWAP Received SIGUSR1, reloading.\n");
 
             doReload();
@@ -214,30 +286,30 @@ namespace
                         return;
                 }
 
-                if (VERBOSE)
+                if (FU_VERBOSE)
                     printf("HOTSWAP Ready to load %s\n", libname);
 
                 // Try load & list, ignore fail, might show up later.
                 Loader l = { libname, nullptr, nullptr };
 
-                if (LOAD_ON_BOOT)
-                    try_reload(l);
+                // if (LOAD_ON_BOOT)
+                //     try_reload(l);
 
                 h.libs.push(l);
             };
 
-            const char* env = getenv("fu_HOTSWAP_lib");
+            const char* env = getenv("FU_HOTSWAP_LIB");
             if (!env)
             {
                 env = "hotswap";
 
-                if (VERBOSE)
-                    printf("HOTSWAP Init, `fu_HOTSWAP_lib` envar is not set, assuming `%s`.\n", env);
+                if (FU_VERBOSE)
+                    printf("HOTSWAP Init, `FU_HOTSWAP_LIB` envar is not set, assuming `%s`.\n", env);
             }
             else
             {
-                if (VERBOSE)
-                    printf("HOTSWAP Init, `fu_HOTSWAP_lib` envar is set to: `%s`.\n", env);
+                if (FU_VERBOSE)
+                    printf("HOTSWAP Init, `FU_HOTSWAP_LIB` envar is set to: `%s`.\n", env);
             }
 
             add_lib(env);
@@ -263,17 +335,32 @@ namespace
 {
     void* ret = fnptr = fallback;
 
-    // Try load immediately.
-    Hotswap& h  = get_inst();
-    Item item   = { symbol, &fnptr };
+    if (FU_HOTSWAP_RELOADING)
+    {
+        // Here we're actually receiving the new symbols,
+        //  we could skip the dlsym stuff altogether
+        //   and do the replacements here.
+        //
+        if (FU_VERBOSE)
+            printf("HOTSWAP Loaded symbol `%s`, ignoring for now, will lookup via dlsym.\n", symbol);
+    }
+    else
+    {
+        if (FU_VERBOSE)
+            printf("HOTSWAP Register symbol `%s`.\n", symbol);
 
-    if (LOAD_ON_BOOT)
-        for (auto& lib: h.libs)
-            if (lib.handle)
-                try_reload(item, lib);
+        // Try load immediately.
+        Hotswap& h  = get_inst();
+        Item item   = { symbol, &fnptr };
 
-    // List.
-    h.items.push(item);
+        // if (LOAD_ON_BOOT)
+        //     for (auto& lib: h.libs.mut())
+        //         if (lib.handle)
+        //             try_reload(item, lib);
+
+        // List.
+        h.items.push(item);
+    }
 
     return ret;
 }
@@ -284,3 +371,6 @@ namespace
 }
 
 #endif
+
+#undef fu_HOTSWAP_use_dladdr
+#undef fu_HOTSWAP_use_rename_hack
